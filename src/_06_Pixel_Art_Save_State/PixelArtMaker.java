278f84120168a1b324cc96b850f6868b0c09927b
Rpod24 <rpodonnell2012@gmail.com>
2021-08-16 13:15:53 -0700

    Finished

---
+++ src/_06_Pixel_Art_Save_State/PixelArtMaker.java
@@ -0,0 +1,127 @@
+package _06_Pixel_Art_Save_State;
+
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class PixelArtMaker implements MouseListener, ActionListener{
+	private JFrame window;
+	private GridInputPanel gip;
+	private GridPanel gp;
+	ColorSelectionPanel csp;
+	JButton sp;
+	private static final String DATA_FILE = "src\\_06_Pixel_Art_Save_State\\saved.dat";
+	
+	public void start() {
+		gip = new GridInputPanel(this);	
+		window = new JFrame("Pixel Art");
+		window.setLayout(new FlowLayout());
+		window.setResizable(false);
+		
+		window.add(gip);
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		if(load()!=null) {
+			loadGridData();
+		}
+	}
+
+	public void submitGridData(int w, int h, int r, int c) {
+		gp = new GridPanel(w, h, r, c);
+		csp = new ColorSelectionPanel();
+		sp = new JButton("Save");
+		sp.addActionListener(this);
+		window.remove(gip);
+		window.add(gp);
+		window.add(csp);
+		window.add(sp);
+		gp.repaint();
+		gp.addMouseListener(this);
+		window.pack();
+	}
+	
+	public void loadGridData() {
+		gp = load();
+		csp = new ColorSelectionPanel();
+		sp = new JButton("Save");
+		sp.addActionListener(this);
+		window.remove(gip);
+		window.add(gp);
+		window.add(csp);
+		window.add(sp);
+		gp.repaint();
+		gp.addMouseListener(this);
+		window.pack();
+	}
+	
+	private static void save(GridPanel data) {
+		try (FileOutputStream fos = new FileOutputStream(new File(DATA_FILE)); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			oos.writeObject(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static GridPanel load() {
+		try (FileInputStream fis = new FileInputStream(new File(DATA_FILE)); ObjectInputStream ois = new ObjectInputStream(fis)) {
+			return (GridPanel) ois.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			// This can occur if the object we read from the file is not
+			// an instance of any recognized class
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		new PixelArtMaker().start();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		gp.setColor(csp.getSelectedColor());
+//		System.out.println(csp.getSelectedColor());
+		gp.clickPixel(e.getX(), e.getY());
+		gp.repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==sp) {
+			save(gp);
+		}
+	}
+}
